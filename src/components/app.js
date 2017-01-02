c322aff1daec566da0ba6ad28274de65d6b5cbf8
mojm <mjm-mars@163.com>
2017-01-02 21:17:16 +0800

    init structure

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var HelloClass = React.createClass({
-	render: function() {
-		return <h1>hello world</h1>;
-	}
-});
+require('../css/app.scss');
+var imageDatas = require('../data/imageDatas.json');
 
-ReactDOM.render(
-	<HelloClass />,
-	document.getElementById('app')
-);
+imageDatas = (function genImageURL(imageDatasArr) {
+	for (var i = 0, j = imageDatasArr.length; i < j; i++) {
+		var singleImageData = imageDatasArr[i];
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+		imageDatasArr[i] = singleImageData;
+	}
+	return imageDatasArr;
+})(imageDatas);
+
+
+
+var Appcomponent = React.createClass({
+	render: function() {
+		return (
+			<section className="stage">
+				<section className="img-sec">
+				</section>
+				<nav className="controller-nav"></nav>
+			</section>
+		);
+	}
+})
+
+Appcomponent.defaultProps = {};
+
+export default Appcomponent;
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const buildPath = path.join(__dirname, './build');
 
 module.exports = {
 	devtool: 'eval-source-map',
-	entry: path.resolve(srcPath, './components/app'),
+	entry: path.resolve(srcPath, 'main.js'),
 	output: {
 		path: buildPath,
 		filename: 'main.js',
@@ -14,6 +14,12 @@ module.exports = {
 	},
 
 	module: {
+		preLoader: [{
+			test: /\.(js|jsx)$/,
+			include: srcPath,
+			loader: 'eslint-loader'
+
+		}],
 		loaders: [{
 				test: /\.js$/,
 				exclude: /node_modules/,
@@ -21,9 +27,24 @@ module.exports = {
 				query: {
 					presets: ['es2015', 'react']
 				}
+			}, {
+				test: /\.css$/,
+				loader: 'style-loader!css-loaderautoprefixer-loader?{browsers: ["last 2 version"]}'
+			}, {
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader?{browsers: ["last 2 version"]}!sass-loader?outputStyle=expanded'
+			}, {
+				test: /\.json$/,
+				loader: 'json-loader'
+			}, {
+				test: /\.(png|jpg|gif|woff|woff2)$/,
+				loader: 'url-loader?limit=8192'
 			}
 
-		]
+		],
+		resolve: {
+			extensions: ['', '.js', 'jsx']
+		}
 	}
 
 }
